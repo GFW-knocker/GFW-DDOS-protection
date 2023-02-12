f423dd8d41dadfd47184b2c11c5a83d57a4a361b
GFW-knocker <123458121+GFW-knocker@users.noreply.github.com>
2023-02-12 11:26:23 +0330

    Add files via upload

---
+++ iptables_portscan.sh
@@ -0,0 +1,12 @@
+### https://unix.stackexchange.com/questions/345114/how-to-protect-against-port-scanners ###
+
+ipset create port_scanners hash:ip family inet hashsize 32768 maxelem 65536 timeout 600
+ipset create scanned_ports hash:ip,port family inet hashsize 32768 maxelem 65536 timeout 60
+
+iptables -A INPUT -m state --state INVALID -j DROP
+iptables -A INPUT -m state --state NEW -m set ! --match-set scanned_ports src,dst -m hashlimit --hashlimit-above 1/hour --hashlimit-burst 5 --hashlimit-mode srcip --hashlimit-name portscan --hashlimit-htable-expire 10000 -j SET --add-set port_scanners src --exist
+iptables -A INPUT -m state --state NEW -m set --match-set port_scanners src -j DROP
+iptables -A INPUT -m state --state NEW -j SET --add-set scanned_ports src,dst
+
+echo "port scan Protection applied!"
+
